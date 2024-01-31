06eb98bc52f5872095075836ea1068fd26279196
sreekanthgiraka <105413920+sreekanthgiraka@users.noreply.github.com>
2024-01-31 15:05:33 +0530

    Update 02-int.py

--- Day-02/examples/02-int.py
+++ Day-02/examples/02-int.py
@@ -1,14 +1,18 @@
 # Integer variables
-num1 = 10
+num1 = 13
 num2 = 5
 
-# Integer Division
+# Integer Division o/p is 2
 result1 = num1 // num2
 print("Integer Division:", result1)
-
-# Modulus (Remainder)
+ 
+# Modulus (Remainder)  
 result2 = num1 % num2
-print("Modulus (Remainder):", result2)
+print("Modulus (Remainder):", result2)  ## o/p is 3
+
+
+result2 = num1 / num2
+print("Regular division:", result2) # Below divison gives exact value 2.6
 
 # Absolute Value
 result3 = abs(-7)
